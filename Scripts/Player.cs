71cf83cf1ae68c7f7fc2f7239b1dd92917f5aa9f
Jorn3355 <RealJornShyti@gmail.com>
2024-10-20 17:16:40 +0200

    Added main and player scenes, player animations, and basic and diagonal movement.

---
+++ Scripts/Player.cs
@@ -0,0 +1,76 @@
+using Godot;
+using System;
+using System.Collections;
+using System.Security.AccessControl;
+using System.Security.Cryptography.X509Certificates;
+using System.Xml.Serialization;
+
+public partial class Player : CharacterBody2D
+{
+
+	public const float Speed = 120;
+	private string playerState; // Will control the player anims via strings so keep the string close to the anims if needed.
+
+	public override void _PhysicsProcess(double delta)
+	{
+		Vector2 dir = Input.GetVector("left", "right", "up", "down"); //Vectors: -x , x, y, -y. They match with the controls.
+
+		if (dir.X == 0 && dir.Y == 0)
+		{
+			playerState = "Idle";
+		}
+		else if (dir.X != 0 || dir.Y != 0)
+		{
+			playerState = "walking";
+		}
+
+		Velocity = dir * Speed;
+		MoveAndSlide();
+		playAnims(dir);
+	}
+
+	public void playAnims(Vector2 dir)
+	{
+		AnimatedSprite2D Sprite = GetNode<AnimatedSprite2D>("AnimSprite");
+		switch (playerState)
+		{
+			case "Idle":
+				Sprite.Play("Idle");
+				break;
+			case "walking":
+				switch(dir.X) {
+					case 1:
+						Sprite.Play("R-walk");
+						break;
+					case -1:
+						Sprite.Play("L-walk");
+						break;
+				}
+				switch(dir.Y) {
+					case 1:
+						Sprite.Play("D-walk");
+						break;
+					case -1:
+						Sprite.Play("U-walk");
+						break;
+				}
+				if (dir.X > 0.5 && dir.Y < -0.5)
+				{
+					Sprite.Play("UR-walk");
+				}
+				else if (dir.X > 0.5 && dir.Y > 0.5)
+				{
+					Sprite.Play("DR-walk");
+				}
+				else if (dir.X < -0.5 && dir.Y > 0.5)
+				{
+					Sprite.Play("DL-walk");
+				}
+				else if (dir.X < -0.5 && dir.Y < -0.5) {
+					Sprite.Play("UL-walk");
+				}
+
+				break;
+		}
+	}
+}
